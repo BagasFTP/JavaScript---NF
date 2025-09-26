da6a2faec79449a50b2cbd823feedbe3a17e7e30
BagasFTP <bagasfirdaustriputra@gmail.com>
2025-09-26 20:37:24 +0700

    Tugas 6

---
+++ Tugas/Tugas6/data.js
@@ -0,0 +1,14 @@
+let data = [
+  { nama: "Bagas", umur: 20, alamat: "Jakarta", email: "bagas@mail.com" },
+  { nama: "Adit", umur: 22, alamat: "Bandung", email: "adit@mail.com" },
+  { nama: "Siti", umur: 19, alamat: "Surabaya", email: "siti@mail.com" },
+  { nama: "Rina", umur: 21, alamat: "Malang", email: "rina@mail.com" },
+  { nama: "Budi", umur: 23, alamat: "Semarang", email: "budi@mail.com" },
+  { nama: "Dewi", umur: 20, alamat: "Jogja", email: "dewi@mail.com" },
+  { nama: "Andi", umur: 22, alamat: "Bekasi", email: "andi@mail.com" },
+  { nama: "Nina", umur: 19, alamat: "Depok", email: "nina@mail.com" },
+  { nama: "Tono", umur: 24, alamat: "Solo", email: "tono@mail.com" },
+  { nama: "Fajar", umur: 25, alamat: "Medan", email: "fajar@mail.com" }
+];
+
+export default data;
